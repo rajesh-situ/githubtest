3fbe13fdead984da93394df5dbf4c2d429a3b098
Poonima V <poonimav@Poonimas-MacBook-Pro.local>
2015-01-07 16:08:39 +0530

    test commit

---
+++ McKinseyPods/McKinseyPods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  McKinseyPods
+//
+//  Created by Poonima V on 1/7/15.
+//  Copyright (c) 2015 McKinsey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ McKinseyPods/McKinseyPods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  McKinseyPods
+//
+//  Created by Poonima V on 1/7/15.
+//  Copyright (c) 2015 McKinsey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
